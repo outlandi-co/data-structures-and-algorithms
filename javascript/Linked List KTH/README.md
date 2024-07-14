feb7805f4026740955e0a2077edebb0c390cfa1c
outlandi-co <outlandico@gmail.com>
2024-07-13 23:09:53 -0700

    added files and test file

---
+++ javascript/Linked List KTH/README.md
@@ -0,0 +1,11 @@
+# Included files
+
+## LinkedList test included
+
+## LinkedList code included
+
+## Screenshot of Figma included
+
+PASS  javascript/Linked List KTH/__tests__/LinkedList.test.js
+  
+  ![LinkedList](<__tests__/Screenshot 2024-07-13 at 11.00.52 PM.png>)
---
+++ javascript/Linked List KTH/__tests__/LinkedList.test.js
@@ -0,0 +1,46 @@
+const { LinkedList } = require('../LinkedList');
+
+describe('LinkedList', () => {
+    it('should return the node value that is k places from the end of the linked list', () => {
+        const ll = new LinkedList();
+        ll.append(1);
+        ll.append(3);
+        ll.append(8);
+        ll.append(2);
+        expect(ll.kthFromEnd(0)).toBe(2);
+        expect(ll.kthFromEnd(2)).toBe(3);
+    });
+
+    it('should throw an error if k is greater than the length of the linked list', () => {
+        const ll = new LinkedList();
+        ll.append(1);
+        ll.append(3);
+        ll.append(8);
+        ll.append(2);
+        expect(() => ll.kthFromEnd(6)).toThrow('k is greater than or equal to the length of the linked list');
+    });
+
+    it('should throw an error if k is not a positive integer', () => {
+        const ll = new LinkedList();
+        ll.append(1);
+        ll.append(3);
+        ll.append(8);
+        ll.append(2);
+        expect(() => ll.kthFromEnd(-1)).toThrow('k must be a non-negative integer');
+    });
+
+    it('should handle linked list of size 1', () => {
+        const ll = new LinkedList();
+        ll.append(1);
+        expect(ll.kthFromEnd(0)).toBe(1);
+    });
+
+    it('should handle the happy path where k is somewhere in the middle', () => {
+        const ll = new LinkedList();
+        ll.append(1);
+        ll.append(3);
+        ll.append(8);
+        ll.append(2);
+        expect(ll.kthFromEnd(1)).toBe(8);
+    });
+});
